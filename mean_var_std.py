2f4402816bd965a61a07157f578b56106ad4d7b5
Alvin <muchellealvin@gmail.com>
2024-05-04 16:17:03 +0000

    finish mean_var_std

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,27 @@
 import numpy as np
 
-def calculate(list):
+def calculate(my_list):
+    calculations = ['mean', 'variance', 'standard deviation', 'max', 'min', 'sum']
+    mean_var_std = {}
+
+    if len(my_list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    np_list = np.array(my_list)
+    np_matrix = np_list.reshape(3, 3)
+
+    mean = np.mean
+    variance = np.var
+    standard_deviation = np.std
+    maximum = np.max
+    minimum = np.min
+    summation = np.sum
+
+    operations = [mean, variance, standard_deviation, maximum, minimum, summation]
+    operands_calculations = zip(operations, calculations)
+    for operand, calculation in operands_calculations:
+        mean_var_std[calculation] = list((operand(np_matrix, 0).tolist(), operand(np_matrix, 1).tolist(), operand(np_matrix)))
+    
+    return mean_var_std
 
 
-
-
-    return calculations
+calculate([0, 1, 2, 3, 4, 5, 6, 7, 8])
